652d39ebc801b3ca38ae3760f51881b970671fbd
DBulii <DBulii@luxoft.com>
2023-02-14 11:57:46 +0200

    add marks

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -12,9 +12,11 @@ class TestHomepage:
         self.page = self.__class__.page = HomepageNav(self.driver)
         self.page.go_to_site()
 
+    @pytest.mark.smoke
     def test_nav_links(self):
         assert self.page.get_nav_links_text() == NAV_LINK_TEXT, 'Wrong tab names'
 
+    @pytest.mark.smoke
     def test_check_go_to_sign_in_form(self):
         self.page.get_sign_in_btn().click()
         self.page.check_sign_in_form_is_present()
